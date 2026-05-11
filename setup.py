f9af388e1c4a490d029d22a7ea5ec8affab61efd
komazon <uuu993421@gmail.com>
2026-05-11 20:06:44 +0900

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(
-    name="nxbt",
+    name="NEO_NXBT",
     include_package_data=True,
     long_description_content_type="text/markdown",
     install_requires=[
